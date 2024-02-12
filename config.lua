57e99d83437c900a6b71561e4ee3dcc9dd943acf
chefrz <116625364+chefrz@users.noreply.github.com>
2024-02-12 22:41:08 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,21 +1,29 @@
 Config = {}
 
-Config.Job = 'burgershot'
+Config.Job = 'police'
 Config.ProgressbarTime = 5000
 Config.Dirt = 10
-Config.SmallBagSellPrice = 2500
-Config.BigBagSellPrice = 5000
+Config.MoneyType = "cash"
+Config.PackageSellPrice = 5000
+Config.DeliveryWait = 5000
 Config.CarSpawnCoord = vector4(-1164.57, -891.6, 14.1, 121.93)
-Config.Car = "nspeedo"
-Config.SmallBagItem = 'burgershot_bagsmall' 
-Config.BigBagItem = 'burgershot_bagbig' 
-Config.GoatBagItem = 'burgershot_baggoat' 
-Config.CoffeeBagItem = 'burgershot_bagcoffe'
+Config.Van = "nspeedo"
+Config.Bike = "foodbike"
+Config.PackageItem = "burgershotbag"
 
-Config.SmallBag = { 'burgershot_bleeder', 'burgershot_colas', 'burgershot_patatos' }
-Config.BigBag = { 'burgershot_bigking', 'burgershot_colab', 'burgershot_patatob' } 
-Config.GoatBag = { 'burgershot_goatwrap', 'burgershot_shotnuggets', 'burgershot_shotrings', 'burgershot_colab' } 
-Config.CoffeeBag = { 'burgershot_coffee', 'burgershot_macaroon' } 
+Config.ItemList = { --Please write packable items here
+    "burgershot_bleeder", 
+    "burgershot_colas", 
+    "burgershot_patatos", 
+    "burgershot_bigking", 
+    "burgershot_colab", 
+    "burgershot_patatob", 
+    "burgershot_goatwrap", 
+    "burgershot_shotnuggets", 
+    "burgershot_shotrings", 
+    "burgershot_coffee",
+    "burgershot_macaroon"
+}
 
 ---------Burger Shot Job Coords---------
 
@@ -24,44 +32,41 @@ Config.Tray =  vector3(-1193.90, -894.37, 14.0)
 Config.Tray2 =  vector3(-1195.26, -892.42, 14.0)
 Config.Storge = vector3(-1197.58, -893.87, 14.0) 
 Config.Fridge = vector3(-1203.42, -895.9, 14.0)
-Config.BurgerStation = vector3(-1199.55, -902.28, 14.0)
+Config.BurgerStation = vector3(-1200.56, -900.9, 14.0)
 Config.Fries = vector3(-1201.52, -899.06, 14.0)
 Config.Drink =  vector3(-1199.57, -895.55, 14.0)
 Config.Garage = vector3(-1165.66, -887.96, 14.13) 
 Config.MeatStation = vector3(-1202.69, -897.46, 14.0)
 Config.PackageStation = vector3(-1196.64, -895.82, 14.0)
 Config.Clean = vector3(-1197.02, -902.17, 14.0)
-Config.SellItem = vector3(-1178.66, -891.76, 13.75)
+Config.SellItem = vector3(-1178.17, -891.55, 13.50)
 Config.Finish = vector3(-1164.57, -891.6, 14.1)
 
----------Burger Shot Job Small Sell Cord---------
+---------Burger Shot Job Package Sell Cord---------
 
-Config.SmallCoords = {
-    [1] = {['x'] = 1061.04, ['y'] = -378.61, ['z'] = 68.24,['h'] = 245.45, ['info'] = 'West Mirror Drive 1'},
-    [2] = {['x'] = 1029.42, ['y'] = -408.96, ['z'] = 65.95,['h'] = 230.45, ['info'] = 'West Mirror Drive 2'},
-    [3] = {['x'] = 1011.27, ['y'] = -422.89, ['z'] = 64.96,['h'] = 230.45, ['info'] = 'West Mirror Drive 3'},
-    [4] = {['x'] = 988.2, ['y'] = -433.74, ['z'] = 63.9,['h'] = 230.45, ['info'] = 'West Mirror Drive 4'},
-    [5] = {['x'] = 967.9, ['y'] = -452.62, ['z'] = 62.41,['h'] = 230.45, ['info'] = 'West Mirror Drive 5'},
-    [6] = {['x'] = 943.26, ['y'] = -463.9, ['z'] = 61.4,['h'] = 123.45, ['info'] = 'West Mirror Drive 6'},
-    [7] = {['x'] = 922.18, ['y'] = -478.69, ['z'] = 61.09,['h'] = 194.45, ['info'] = 'West Mirror Drive 7'},
-    [8] = {['x'] = 906.58, ['y'] = -489.69, ['z'] = 59.44,['h'] = 194.45, ['info'] = 'West Mirror Drive 8'},
-    [9] = {['x'] = 878.99, ['y'] = -498.51, ['z'] = 57.88,['h'] = 232.45, ['info'] = 'West Mirror Drive 9'},
-    [10] = {['x'] = 862.28, ['y'] = -509.58, ['z'] = 57.33,['h'] = 232.45, ['info'] = 'West Mirror Drive 10'},
-}
-
----------Burger Shot Job Big Sell Cord---------
-
-Config.BigCoords = {
-    [1] = {['x'] = 224.15, ['y'] = 513.55, ['z'] = 140.92,['h'] = 245.45, ['info'] = 'Vinewood 1'},
-    [2] = {['x'] = 43.02, ['y'] = 468.85, ['z'] = 148.1,['h'] = 230.45, ['info'] = 'Vinewood 2'}, 
-    [3] = {['x'] = 119.33, ['y'] = 564.1, ['z'] = 183.96,['h'] = 230.45, ['info'] = 'Vinewood 3'},
-    [4] = {['x'] = -60.82, ['y'] = 360.56, ['z'] = 113.06,['h'] = 230.45, ['info'] = 'Vinewood 4'},
-    [5] = {['x'] = -622.87, ['y'] = 488.81, ['z'] = 108.88,['h'] = 230.45, ['info'] = 'Vinewood 5'}, 
-    [6] = {['x'] = -1040.67, ['y'] = 508.11, ['z'] = 84.38,['h'] = 123.45, ['info'] = 'Vinewood 6'}, 
-    [7] = {['x'] = -1308.13, ['y'] = 448.9, ['z'] = 100.97,['h'] = 194.45, ['info'] = 'Vinewood 7'}, 
-    [8] = {['x'] = -1733.21, ['y'] = 378.99, ['z'] = 89.73,['h'] = 194.45, ['info'] = 'Vinewood 8'},
-    [9] = {['x'] = -2009.15, ['y'] = 367.42, ['z'] = 94.81,['h'] = 232.45, ['info'] = 'Vinewood 9'},
-    [10] = {['x'] = -1996.29, ['y'] = 591.25, ['z'] = 118.1,['h'] = 232.45, ['info'] = 'Vinewood 10'},
+Config.Locations = { 
+    ["coords"] = {
+        [1] = vector3(224.15, 513.55, 140.92),
+        [2] = vector3(43.02, 468.85, 148.1),
+        [3] = vector3(119.33, 564.1, 183.96),
+        [4] = vector3(-60.82, 360.56, 113.06),
+        [5] = vector3(-622.87, 488.81, 108.88),
+        [6] = vector3(-1040.67, 508.11, 84.38),
+        [7] = vector3(-1308.13, 448.9, 100.97),
+        [8] = vector3(-1733.21, 378.99, 89.73),
+        [9] = vector3(-2009.15, 367.42, 94.81),
+        [10] = vector3(-1996.29, 591.25, 118.1),      
+        [11] = vector3(1061.04, -378.61, 68.24),
+        [12] = vector3(1029.42, -408.96, 65.95),
+        [13] = vector3(1011.27, -422.89, 64.96),
+        [14] = vector3(988.2, -433.74, 63.9),
+        [15] = vector3(967.9, -452.62, 62.41),
+        [16] = vector3(943.26, -463.9, 61.4),
+        [17] = vector3(922.18, -478.69, 61.09),
+        [18] = vector3(906.58, -489.69, 59.44),
+        [19] = vector3(878.99, -498.51, 57.88),
+        [20] = vector3(862.28, -509.58, 57.33),      
+    },
 }
 
 ---------Burger Shot Job Create Item List---------
@@ -70,36 +75,30 @@ Config.BleederBurger = "burgershot_bleeder"
 Config.BigKingBurger = "burgershot_bigking"
 Config.Wrap = "burgershot_goatwrap"
 Config.Macaroon = "burgershot_macaroon"
-
 Config.Tomato = 'burgershot_tomato' 
 Config.Cheddar = 'burgershot_cheddar'
 Config.VegetableCurly = 'burgershot_curly'
 Config.Sauce = 'burgershot_sauce' 
 Config.Bread = 'burgershot_bread' 
 Config.Lavash = 'burgershot_lavash' 
-
 Config.SmallColaItem = 'burgershot_colas' 
 Config.BigColaItem = 'burgershot_colab' 
 Config.CoffeeItem = 'burgershot_coffee'
-
 Config.BigEmptyCardboard = 'burgershot_bigcardboard' 
 Config.SmallEmptyCardboard = 'burgershot_smallcardboard' 
 Config.SmallEmptyGlass = 'burgershot_smallemptyglass' 
 Config.BigEmptyGlass = 'burgershot_bigemptyglass' 
 Config.CoffeeEmptyGlass = 'burgershot_coffeeemptyglass' 
-
 Config.FrozenMeat = 'burgershot_frozenmeat' 
 Config.FrozenRings = 'burgershot_frozenrings' 
 Config.FrozenNuggets = 'burgershot_frozennuggets' 
 Config.SmallFrozenPotato = 'burgershot_smallfrozenpotato' 
 Config.BigFrozenPotato = 'burgershot_bigfrozenpotato'
-
 Config.Meat = 'burgershot_meat' 
 Config.Rings = 'burgershot_shotrings' 
 Config.Nuggets = 'burgershot_shotnuggets' 
 Config.SmallPotato = 'burgershot_patatos' 
 Config.BigPotato = 'burgershot_patatob' 
-
 Config.Cone = "burgershot_icecreamcone"
 Config.ChocolateIceCream = "burgershot_chocolateicecream"
 Config.VanillaIceCream = "burgershot_vanillaicecream"
@@ -159,7 +158,7 @@ Config.Shop = {
             slot = 6,
         },
         {
-            name = "burgershot_bigcardboard",-----
+            name = "burgershot_bigcardboard",
             amount = 10,
             type = "item",
             price = 200,
